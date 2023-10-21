a30c125d294d015f4d5453d059e9830ff514241f
Addi Kamal <kamal.addi@proton.me>
2023-10-21 16:25:50 +0100

    Add test case to handle empty strings

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require("../scripts/capitalize.js");
 test("Capitalizes a word", () => {
   expect(capitalize("hello")).toBe("Hello");
 });
+
+test("Works with empty strings", () => {
+  expect(capitalize("")).toBe("");
+});
