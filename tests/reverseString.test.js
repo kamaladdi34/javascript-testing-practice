3a076eed204a5ef23fca044bf352586e41a18061
Addi Kamal <kamal.addi@proton.me>
2023-10-21 19:30:17 +0100

    Add test case to reverseString for numbers

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require("../scripts/reverseString");
 test("Reverses a string", () => {
   expect(reverseString("Hello")).toBe("olleH");
 });
+
+test("Works with numbers", () => {
+  expect(reverseString(12345)).toBe("54321");
+});
