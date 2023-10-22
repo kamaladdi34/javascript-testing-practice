5ff62d988edba456061cd9a95e6095e0428a0021
Addi Kamal <kamal.addi@proton.me>
2023-10-22 10:02:59 +0100

    Create more tests for calculator

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,7 @@
 const calculator = {
   add: (a, b) => a + b,
+  substract: (a, b) => a - b,
+  devide: (a, b) => a / b,
+  multiply: (a, b) => a * b,
 };
 module.exports = calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require("../scripts/calculator");
 test("Adds two numbers", () => {
   expect(calculator.add(1, 1)).toBe(2);
 });
+
+test("Substracts two numbers", () => {
+  expect(calculator.substract(1, 1)).toBe(0);
+});
+
+test("Multiplies two numbers", () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+});
+
+test("Devides two numbers", () => {
+  expect(calculator.devide(2, 2)).toBe(1);
+});
