0a0d903e42a725846423bc3b1ce42f1ecb434736
Addi Kamal <kamal.addi@proton.me>
2023-10-22 11:17:46 +0100

    Add test to check if caesarCipher wraps around

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,6 +1,6 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 function caesarCipher(string, offset) {
-  return alphabet[alphabet.indexOf(string) + offset];
+  return alphabet[(alphabet.indexOf(string) + offset) % alphabet.length];
 }
 
 module.exports = caesarCipher;
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -3,3 +3,7 @@ const caesarCipher = require("../scripts/caesarCipher");
 test("Works with a single letter", () => {
   expect(caesarCipher("a", 1)).toBe("b");
 });
+
+test("Wraps when index out of range", () => {
+  expect(caesarCipher("z", 13)).toBe("m");
+});
